807d06c57862e67062e89e77a7dae5c8044d36ef
Michelle Manzogit config --global user.email memanzo17@gmail.com <memanzo17@gmail.com>
2018-02-24 15:40:01 -0500

    added basic functionality

---
+++ app.js
@@ -0,0 +1,40 @@
+const movieSearchURL = 'https://api.themoviedb.org/3/search/movie'
+
+function getAPIData(searchTerm, callback) {
+	const search = {
+		api_key: '8541c092938098d21b11f58a14dd114e',
+		query: `${searchTerm}`
+	}
+
+	$.getJSON(movieSearchURL, search, callback)
+}
+
+
+
+
+//front link for poster- https://image.tmdb.org/t/p/w1280
+
+
+function displayTMDBSearchData(data) {
+	const results = `
+	<div class="result-container">
+		<h2>${data.results[0].title}</h2>
+		<img src="https://image.tmdb.org/t/p/w1280${data.results[0].poster_path}">
+	</div>
+	`;
+	$('.js-search-results').html(results);
+}
+
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const searchTarget = $(event.currentTarget).find('.js-search');
+    const search = searchTarget.val();
+    // clear out the input
+    searchTarget.val("");
+    getAPIData(search, displayTMDBSearchData);
+  });
+}
+
+$(watchSubmit);
