cd3b519b717fe27292ad3010af276a54ee3b084d
Michelle Manzogit config --global user.email memanzo17@gmail.com <memanzo17@gmail.com>
2018-02-25 09:25:02 -0500

    added store for movie data

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-const movieSearchURL = 'https://api.themoviedb.org/3/search/movie'
+const STORE = {
+	movieData: null,
+	idData: null
+}
 
+function storeMovieData(data) {
+	console.log(data)
+	STORE.movieData = data
+	console.log('store data ran')
+	console.log(STORE.movieData)
+}
+
+function storeIdData(data) {
+	STORE.idData = data
+}
+
+//Build URL to request API data from. Second and Third Cats MUST include / at beginning of parameter when function called. 
+function buildURL(firstCat, secondCat, thirdCat) {
+	return `https://api.themoviedb.org/3/${firstCat}${secondCat}${thirdCat}`
+}
+
+//Get movie data from search. Second and Third Cats MUST include / at beginning of parameter when function called.
 function getAPIData(searchTerm, callback) {
+	const movieSearchURL = buildURL('search', '/movie', '');
 	const search = {
 		api_key: '8541c092938098d21b11f58a14dd114e',
 		query: `${searchTerm}`
 	}
-
 	$.getJSON(movieSearchURL, search, callback)
 }
 
-function displayTMDBSearchData(data) {
+
+function getMovieIdData (searchTerm, callback) {
+	const movieIdSearchURL = buildURL('movie/', '157336', '')
+	const search = {
+		api_key: '8541c092938098d21b11f58a14dd114e',
+	}
+	$.getJSON(movieIdSearchURL, search, callback)
+}
+
+function displayIdData (data){
+	const results = `<p>Runtime: ${data.runtime}</p>`
+	$('.js-search-results-id').html(results);
+}
+
+function displayMovieData(data) {
 	const results = `
-	<div class="result-container">
+		<div class="result-container">
 		<h2>${data.results[0].title}</h2>
 		<p>Plot Summary: ${data.results[0].overview}</p>
-		<p>Release date: ${data.results[0].release_date}</p>
+		<p>Average rating: ${data.results[0].vote_average}/10</p>
 		<img src="https://image.tmdb.org/t/p/w1280${data.results[0].poster_path}" alt="${data.results[0].title}">
 	</div>
 	`;
@@ -29,8 +63,10 @@ function watchSubmit() {
     const search = searchTarget.val();
     // clear out the input
     searchTarget.val("");
-    getAPIData(search, displayTMDBSearchData);
+    getAPIData(search, storeMovieData);
+    getMovieIdData(search, storeIdData);
   });
+
 }
 
 $(watchSubmit);
