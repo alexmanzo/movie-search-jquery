842f359c72fc2b4f87bf830f5605a67988e250d7
Michelle Manzogit config --global user.email memanzo17@gmail.com <memanzo17@gmail.com>
2018-02-26 22:47:16 -0500

    completed javascript functonality

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const STORE = {
-	movieData: null,
-	idData: null
-}
+$(window).on('load', function(){
+// Instantiate the Bloodhound suggestion engine
+const movies = new Bloodhound({
+    datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: `http://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=8541c092938098d21b11f58a14dd114e`,
+        wildcard: '%QUERY',
+        filter: function (movies) {
+            return $.map(movies.results, function (movie) {
+                return {
+                    value: movie.title
+                };
+            });
+        }
+    }
+});
+
+// Initialize the Bloodhound suggestion engine
+movies.initialize();
+// Instantiate the Typeahead UI
+$('.typeahead').typeahead(null, {
+    displayKey: 'value',
+    source: movies.ttAdapter(),
+    templates: {
+        suggestion: Handlebars.compile("<p style='padding:6px'><b>{{value}}</b></p>"),
+        footer: Handlebars.compile("<b>Searched for '{{query}}'</b>")
+    }
+});
+});
 
 //Build URL to request API data from. Second and Third Cats MUST include / at beginning of parameter when function called. 
 function buildURL(firstCat, secondCat, thirdCat) {
@@ -18,33 +46,49 @@ function getAPIData(searchTerm, callback) {
     },
     dataType: 'json',
     type: 'GET',
-    success: callback
+    success: function(data){
+                const idSettings = {
+                url: buildURL('movie/', `${data.results[0].id}`, ''),
+                data: {
+                api_key: '8541c092938098d21b11f58a14dd114e'
+                },
+                dataType: 'json',
+                type: 'GET',
+                success: callback
+                };
+              $.ajax(idSettings)  
+              } 
   };
 
   $.ajax(settings);
 }
 
-function renderResult(result) {
-	return `
-		<div class="result-container">
-		<h2>${result.title}</h2>
-		<p>Plot Summary: ${result.overview}</p>
-		<p>Average rating: ${result.vote_average}/10</p>
-		<img src="https://image.tmdb.org/t/p/w1280${result.poster_path}" alt="${result.title}">
-	</div>
-	`;
-}
+
 
 function displayMovieData(data) {
-	const results = data.results.map((item, index) => renderResult(item));
+	const results = `
+    <div class="result-container">
+    <h2>${data.title}</h2>
+    <h3>${data.tagline}</h3>
+    <p>Year Released: ${data.release_date.substring(0,4)}</p>
+    <p>Genres: ${data.genres.map(function(genre) {return ` ${genre.name}`})}</p>
+    <p>Plot Summary: ${data.overview}</p>
+    <p>Runtime: ${data.runtime} minutes</p>
+    <p>Budget: $${data.budget.toLocaleString()}</p>
+    <p>Revenue: $${data.revenue.toLocaleString()}</p>
+    <img src="https://image.tmdb.org/t/p/w1280${data.poster_path}" alt="${data.title}" id="poster">
+    <img src="https://image.tmdb.org/t/p/w1280${data.backdrop_path}" alt="${data.title}" id="backdrop">
+  </div>
+  `;
 	$('.js-search-results').html(results);
+  console.log(data)
 }
 
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    const searchTarget = $(event.currentTarget).find('.js-search');
+    const searchTarget = $(event.currentTarget).find('#query');
     const search = searchTarget.val();
     searchTarget.val("");
     getAPIData(search, displayMovieData);
